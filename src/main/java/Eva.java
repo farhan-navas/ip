d6de57e9ba60e04e02e3d4a8561c566b9a7da5c3
farhan-navas <farhanmnavas@gmail.com>
2025-01-24 15:26:11 +0800

    complete eva lvl-0

---
+++ src/main/java/Eva.java
@@ -0,0 +1,6 @@
+public class Eva {
+    public static void main(String[] args) {
+        String intro = "Hello! I'm Eva \nWhat can I do for you? \nBye. Hope to see you again soon!";
+        System.out.println(intro);
+    }
+}
