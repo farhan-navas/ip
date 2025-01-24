31c2071dbac5699c5f81c555aaef514b26502bed
farhan-navas <farhanmnavas@gmail.com>
2025-01-24 15:41:24 +0800

    complete level-1

--- src/main/java/Eva.java
+++ src/main/java/Eva.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Eva {
     public static void main(String[] args) {
-        String intro = "Hello! I'm Eva \nWhat can I do for you? \nBye. Hope to see you again soon!";
+        Scanner scanner = new Scanner(System.in);
+        String intro = "Hello! I'm Eva \nWhat can I do for you? \n";
+        String end = "Bye. Hope to see you again soon!";
         System.out.println(intro);
+
+        String userInput;
+        while (true) {
+            userInput = scanner.nextLine();
+            if (userInput.equals("bye")) {
+                break;
+            }
+            System.out.println(userInput);
+        }
+
+        System.out.println(end);
+        scanner.close();
     }
 }
