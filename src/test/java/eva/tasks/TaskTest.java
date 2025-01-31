286765a95b78a734aeb9d1e64ee49c2a9564a1d6
farhan-navas <farhanmnavas@gmail.com>
2025-01-31 08:46:29 +0800

    use gradle as build tool

---
+++ src/test/java/eva/tasks/TaskTest.java
@@ -0,0 +1,4 @@
+package eva.tasks;
+
+public class TaskTest {
+}
---
+++ src/test/java/eva/ui/UiTest.java
@@ -0,0 +1,47 @@
+package eva.ui;
+
+import eva.exceptions.TaskException;
+import eva.tasks.Task;
+import eva.tasks.Todo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class UiTest {
+    private Ui ui;
+    private ArrayList<Task> taskList;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        ui = new Ui();
+        taskList = new ArrayList<>();
+        System.setOut(new PrintStream(outContent));
+    }
+
+    // Test for handleInput addTask
+    @Test
+    public void testHandleInput_addTask() throws TaskException {
+        String simInput = "todo read book";
+        System.setIn(new ByteArrayInputStream(simInput.getBytes()));
+
+        ui.handleInput(new Scanner(System.in), taskList);
+
+        assertEquals(1, taskList.size());
+        assertTrue(this.taskList.get(0) instanceof Todo);
+        assertEquals("read book", this.taskList.get(0).getName());
+
+        String consoleOutput = outContent.toString();
+        assert consoleOutput.contains("Got it. I've added this task:");
+        assert consoleOutput.contains("[T][ ] read book");
+        assert consoleOutput.contains("Now you have 1 task in the list.");
+    }
+}
